4ab17811c60be829e43d27c60690949b90e741d1
francesco-giorgione <104784912+francesco-giorgione@users.noreply.github.com>
2023-01-04 12:48:37 +0100

    Create select.php

---
+++ select.php
@@ -0,0 +1,28 @@
+<?php
+    $serverName = "ilike.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "iLike", // update me
+        "Uid" => "CloudSAe14a0d66", // update me
+        "PWD" => "iLike_2022" // update me
+    );
+
+    echo 'Ora provo a connettermi al db';    
+
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "select * from Contenuti";
+    $getResults= sqlsrv_query($conn, $tsql);
+
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo "PROBLEMA";
+    else
+        echo "OK";
+
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ("Nuovo risultato");
+    }
+
+    echo 'Sono alla fine';
+
+?>
