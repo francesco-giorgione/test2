0c95e816c9f264b1891b6e12bfbdef973b924f2a
francesco-giorgione <104784912+francesco-giorgione@users.noreply.github.com>
2023-01-04 13:19:23 +0100

    Update select.php

--- select.php
+++ select.php
@@ -5,15 +5,14 @@
         "Uid" => "CloudSAe14a0d66", // update me
         "PWD" => "iLike_2022" // update me
     );
-
-    echo 'Ora provo a connettermi al db';    
+   
 
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "select * from Contenuti";
+	$tsql = $_POST['query'];
     $getResults= sqlsrv_query($conn, $tsql);
 
-    echo ("Reading data from table" . PHP_EOL);
+
     if ($getResults == FALSE)
         echo "PROBLEMA";
     else
