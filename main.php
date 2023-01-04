056b369e64d5c653337beea181ea1536a224512c
francesco-giorgione <104784912+francesco-giorgione@users.noreply.github.com>
2023-01-04 11:29:24 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,3 +1,13 @@
 <?php
-echo "Hello";
+$db_host = "ilike.database.windows.net";
+$db_name = " iLike";
+$db_user = "CloudSAe14a0d66";
+$db_password = " iLike_2022";
+
+//connessione al database
+$db = mysql_connect($db_host, $db_user, $db_password);
+
+if ($db == FALSE) die ("Errore nella connessione. Verificare i parametri nel file connection.php");
+else echo "Connessione effettuata";
+
 ?>
