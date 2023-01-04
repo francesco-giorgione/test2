d1298a7941c2264dae4e0c8b41353770ac22fd21
francesco-giorgione <104784912+francesco-giorgione@users.noreply.github.com>
2023-01-04 13:53:09 +0100

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,22 @@
+
+<?php
+	$serverName = "ilike.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "iLike", // update me
+        "Uid" => "CloudSAe14a0d66", // update me
+        "PWD" => "iLike_2022" // update me
+    );
+   
+
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+	$tsql = $_POST['query'];
+	
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
+	$conn->close();
+?>
