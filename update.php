0f6f5272be9c7bba2653e7c5c7deb984464941b0
francesco-giorgione <104784912+francesco-giorgione@users.noreply.github.com>
2023-01-04 15:22:55 +0100

    Create update.php

---
+++ update.php
@@ -0,0 +1,26 @@
+
+<?php
+	$serverName = "ilike.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "iLike", // update me
+        "Uid" => "CloudSAe14a0d66", // update me
+        "PWD" => "iLike_2022" // update me
+    );
+   
+
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+	$tsql = $_POST['query'];
+    $stmt= sqlsrv_query($conn, $tsql);
+	
+	if ($stmt) {  
+		echo "true"; 
+	} else {  
+		echo "false"; 
+	}  
+	  
+	/* Free statement and connection resources. */  
+	sqlsrv_close($conn); 
+	sqlsrv_free_stmt($stmt);  
+	
+?>  
