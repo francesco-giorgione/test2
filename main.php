32d12b95fbbf6fabca5cd7c1f3735309e11857a2
francesco-giorgione <104784912+francesco-giorgione@users.noreply.github.com>
2023-01-04 11:48:38 +0100

    Update main.php

--- main.php
+++ main.php
@@ -4,10 +4,10 @@ $db_name = "iLike";
 $db_user = "CloudSAe14a0d66";
 $db_password = "iLike_2022";
 
-echo "Sto provando a connettermi al db";
 
 //connessione al database
 $db = mysql_connect($db_host, $db_user, $db_password);
+echo "Sto provando a connettermi al db";
 
 if ($db == FALSE) die ("Errore nella connessione. Verificare i parametri nel file connection.php");
 else echo "Connessione effettuata";
